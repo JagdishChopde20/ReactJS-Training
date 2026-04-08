9ea0430e6c84cd96fd241f28ab3cf5a45ec33f11
JagdishChopde20 <jagdishchopde@outlook.com>
2026-04-08 15:43:21 +0530

    New Header component added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Header from './components/header/Header'
 
 function App() {
+  // js code
 
   return (
     <div>
-      <h1>React Training</h1>
+      <Header />
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id culpa ipsum iusto doloribus, totam explicabo ex facilis cumque fugiat, distinctio eveniet illum. Iure minima provident dicta tenetur molestias! Corporis, debitis.</p>
     </div>
   )
