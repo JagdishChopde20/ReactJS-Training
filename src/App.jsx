667394af077141475bc28fad159369900ca4446d
JagdishChopde20 <jagdishchopde@outlook.com>
2026-04-08 10:14:53 +0530

    Initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>React Training</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id culpa ipsum iusto doloribus, totam explicabo ex facilis cumque fugiat, distinctio eveniet illum. Iure minima provident dicta tenetur molestias! Corporis, debitis.</p>
+    </div>
+  )
+}
+
+export default App
